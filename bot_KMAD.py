18c50f8620be3bd9fdc070a057118d8175c3649c
Valentyna Pikalova <vpikalova@hotmail.com>
2023-04-12 05:16:21 +0300

    Update bot_KMAD.py

--- bot_KMAD.py
+++ bot_KMAD.py
@@ -221,11 +221,14 @@ def perevagu(update: Update, context: CallbackContext):
         content_lines[29:41]), parse_mode="HTML")
     query.message.reply_text(text='\n'.join(
         content_lines[41:60]), parse_mode="HTML") 
+    query.message.reply_photo(link + photos[1])
     query.message.reply_text(text='\n'.join(
         content_lines[60:]), parse_mode="HTML")  
-    query.message.reply_photo(link + photos[1])
-    #reply = InlineKeyboardMarkup(keyboard_backto_kafedra)
+    
+
     query.message.reply_text(text=content_lines[-1], reply_markup=reply, parse_mode="HTML")
+    
+    #reply = InlineKeyboardMarkup(keyboard_backto_kafedra)
     #query.message.reply_text(text=content, reply_markup=reply, parse_mode="HTML")
     
 
